3c384e4501132c18fbe546fbd79462584fbb72a2
idelfrides <idelfridesjorgepapai@gmail.com>
2020-01-02 19:24:26 -0300

    Making redirect with js

---
+++ app/static/js/todolist_functions.js
@@ -0,0 +1,31 @@
+
+
+function go2register() {
+    window.location.href = "http://127.0.0.1:5000/register";
+
+    /* window.location.replace("http://127.0.0.1:5000/register"); */
+}
+
+function go2login() {
+    /* window.location.href = "http://127.0.0.1:5000/login"; */
+    window.location.replace("http://127.0.0.1:5000/login");
+}
+
+
+
+
+
+
+
+
+
+/*
+$(document).ready(function () {
+    $('#updateTaskModal1').modal('show');
+});
+*/
+/*
+$(window).load(function (){
+    $('#updateTaskModal1').modal('show');
+}); */
+
