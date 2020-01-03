02158d380fc7db2b2e9b437fc81dda4a95abda9e
idelfrides <idelfridesjorgepapai@gmail.com>
2020-01-03 10:58:08 -0300

    Add a real secret_key

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'todolist_db.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SECRET_KEY = 'um-nome-bem-seguro'
+SECRET_KEY = b'\xdd\x8b\xad\x13\xf8\xda-\xfd\xa2c\\B\x1a\xed4\xb1'
